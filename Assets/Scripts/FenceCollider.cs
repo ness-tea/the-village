c446a1b0c80f42f24692bfae69bec520aa0dad60
Vanessa Truong <truonv1@mcmaster.ca>
2018-03-07 14:13:59 -0500

    Re-adding assets folder with visible meta files and force text

---
+++ Assets/Scripts/FenceCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceCollider : MonoBehaviour {
+	
+	public Collider2D fence;
+	public int fenceCount;
+
+	// Use this for initialization
+	void Start () {
+		fence = GetComponent<Collider2D>();
+		fenceCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fenceCount == 5)
+		{
+		    fence.enabled = false;
+		    Debug.Log("Collider2D.enabled = " + fence.enabled);
+		}
+	}
+}
